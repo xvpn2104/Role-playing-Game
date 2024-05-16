dfd21bfabf9100defe56359cbf8306068dba7a07
xvpn2104 <158258246+xvpn2104@users.noreply.github.com>
2024-05-16 23:34:00 +0400

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+{
+    name: "stick",
+    power: 5,
+  },
+  {
+    name:"dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  }
+];
 const locations = [
    {
     name: "town square",
@@ -79,9 +97,33 @@ function buyHealth() {
   health += 10;
 goldText.innerText = gold;
   healthText.innerText = health;
-}}
+} else {
+text.innerText = "You do not have enough gold to buy health."
+  }
+}
 function buyWeapon() {
-
+if(currentWeapon < weapons.length-1){
+if (gold >= 30){
+gold -= 30;
+currentWeapon ++;
+goldText.innerText = gold;
+ let newWeapon = weapons[currentWeapon].name;
+    text.innerText = "You now have a "+ newWeapon+ ".";
+inventory.push(newWeapon);
+text.innerText += " In your inventory you have: "+ inventory;
+}else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
+}
+}else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }}
+function sellWeapon(){
+if (inventory.length > 1) {
+gold += 15;
+goldText.innerText = gold;
+let currentWeapon; }  
 }
 function fightSlime(){
 
