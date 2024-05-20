527b379a9834bac7cd99786923ec17732aed4c85
xvpn2104 <158258246+xvpn2104@users.noreply.github.com>
2024-05-20 18:10:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -80,9 +80,9 @@ const locations = [
 }
 ];
 // initialize buttons
-button1.onclick() = goStore;
-button2.onclick() = goCave;
-button3.onclick() = fightDragon;
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 
 
 function update(location) {
